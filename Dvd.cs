06c1ba5f09fc43943118a5176ec55099ce527fba
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2023-10-12 09:53:25 +0200

    Add project files.

---
+++ Dvd.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolcsonzoOOP
+{
+    internal class Dvd : IKolcsonozheto, IComparable<Dvd>
+    {
+        private List<Dvd> dvdk;
+        private string cim;
+        private int hossz;
+
+        public Dvd(string cim, int hossz)
+        {
+            this.cim = cim;
+            this.hossz = hossz;
+            Dvdk = new List<Dvd>();
+           
+        }
+
+        public List<Dvd> Dvdk { get => dvdk; set => dvdk = value; }
+
+        public int MeddigKolcsonozheto()
+        {
+            return 7;
+        }
+
+        public string MegjelenitendoNev()
+        {
+            return this.cim + " ("+this.hossz+")";
+        }
+        public void fillDvd()
+        {
+            this.Dvdk.Add(new Dvd("Titanic", 300));
+            this.Dvdk.Add(new Dvd("Saw", 230));
+            this.Dvdk.Add(new Dvd("LongDvd", 400));
+            this.Dvdk.Add(new Dvd("ShortDvd", 100));
+            this.Dvdk.Add(new Dvd("AAAAA", 310));
+        }
+        public override string ToString()
+        {
+            return MegjelenitendoNev() + " " + MeddigKolcsonozheto();
+        }
+
+            public int CompareTo(Dvd dvd1)
+            {
+                if (this.hossz < dvd1.hossz) { return -1; }
+                else if (this.hossz > dvd1.hossz) { return 1; }
+                else { return 0; }
+            }
+        
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using KolcsonzoOOP;
+
+List<IKolcsonozheto> katalogus = new List<IKolcsonozheto>();
+katalogus.Add(new Konyv("Game of Thrones","George R.R Martin","is12aa23s"));
+katalogus.Add(new Dvd("Star Wars", 120));
+katalogus.Add(new Ujsag("Magyar Hírlap",2003,30));
+
+Dvd dvd = new Dvd("Lord of THe Rings", 300);
+dvd.fillDvd();
+dvd.Dvdk.Sort();
+/*foreach (var k in katalogus)
+{
+    Console.WriteLine(k);
+}*/
+
+foreach  (var d in dvd.Dvdk)
+{
+    Console.WriteLine(d);
+}
